1a7e45a70c0613b79de3df40b7b1a52b922a6a4c
memo <379902044@qq.com>
2026-05-03 16:52:21 +0800

    🔥 chore: remove non-fixture files from example_sch
    Co-Authored-By: GLM <noreply@z.ai>

--- example_sch/download_iframe.js
+++
@@ -1,7 +0,0 @@
-const iframe = $x('//body//*[@id="ecad-viewer-div"]/iframe')[0];
-const html = iframe.contentDocument.documentElement.outerHTML;
-const blob = new Blob([html], {type: 'text/html'});
-const a = document.createElement('a');
-a.href = URL.createObjectURL(blob);
-a.download = 'page.html';
-a.click();
